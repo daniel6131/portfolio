af82125b2bceb3cd90c066676b9027524a93102f
daniel.obrien <daniel.obrien@piratech.io>
2025-07-04 16:40:54 +1000

    feat(projects): adding interactable project section with overlay

---
+++ src/components/Project.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import ProjectDetails from './ProjectDetails';
+
+const Project = ({ title, description, subDescription, href, image, tags, setPreview }) => {
+  const [isHidden, setIsHidden] = useState(false);
+  return (
+    <>
+      <div
+        className='flex-wrap items-center justify-between space-y-14 py-10 sm:flex sm:space-y-0'
+        onMouseEnter={() => setPreview(image)}
+        onMouseLeave={() => setPreview(null)}
+      >
+        <div>
+          <p className='text-2xl'>{title}</p>
+          <div className='text-sand mt-2 flex gap-5'>
+            {tags.map(tag => (
+              <span key={tag.id}>{tag.name}</span>
+            ))}
+          </div>
+        </div>
+        <button
+          onClick={() => setIsHidden(true)}
+          className='hover-animation flex cursor-pointer items-center gap-1'
+        >
+          Read More
+          <img alt='' src='assets/arrow-right.svg' className='w-5' />
+        </button>
+      </div>
+      <div className='h-[1px] w-full bg-gradient-to-r from-transparent via-neutral-700 to-transparent' />
+      {isHidden && (
+        <ProjectDetails
+          title={title}
+          description={description}
+          subDescription={subDescription}
+          image={image}
+          tags={tags}
+          href={href}
+          closeModal={() => setIsHidden(false)}
+        />
+      )}
+    </>
+  );
+};
+
+export default Project;
---
+++ src/sections/Projects.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import Project from '../components/Project';
+import { myProjects } from '../constants';
+import { motion, useMotionValue, useSpring } from 'motion/react';
+const Projects = () => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const springX = useSpring(x, { damping: 10, stiffness: 50 });
+  const springY = useSpring(y, { damping: 10, stiffness: 50 });
+  const handleMouseMove = e => {
+    x.set(e.clientX + 20);
+    y.set(e.clientY + 20);
+  };
+  const [preview, setPreview] = useState(null);
+  return (
+    <section onMouseMove={handleMouseMove} className='c-space section-spacing relative'>
+      <h2 className='text-heading'>My Selected Projects</h2>
+      <div className='mt-12 h-[1px] w-full bg-gradient-to-r from-transparent via-neutral-700 to-transparent' />
+      {myProjects.map(project => (
+        <Project key={project.id} {...project} setPreview={setPreview} />
+      ))}
+      {preview && (
+        <motion.img
+          className='pointer-events-none fixed top-0 left-0 z-50 h-56 w-80 rounded-lg object-cover shadow-lg'
+          src={preview}
+          style={{ x: springX, y: springY }}
+        />
+      )}
+    </section>
+  );
+};
+
+export default Projects;
